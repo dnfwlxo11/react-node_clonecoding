b9da6b270090252d0a8f8b6df9175c3b3fa967ea
daein <dnfwlxo11@naver.com>
2020-12-29 11:30:24 +0900

    add: Movie Detail information

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'cb09d58b7c04c5a7d23e368d6b4f6a87';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
