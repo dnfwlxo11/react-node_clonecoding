de7719138372a476bcf02666a0040db0ca059b06
daein <dnfwlxo11@naver.com>
2020-12-31 16:13:12 +0900

    add: favorite page + popover func

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -20,4 +20,49 @@ router.post('/favoriteNumber', (req, res) => {
     // send favorite number to front
 })
 
+router.post('/favorited', (req, res) => {
+    // already like this movie
+    Favorite.find({ "movieId": req.body.movieId, "userFrom": req.body.userFrom })
+    .exec((err, info) => {
+        if (err) return res.status(400).send(err)
+
+        let result = false;
+        if (info.length != 0 ) {
+            result = true;
+        }
+
+        res.status(200).json({ success:true, favorited:result})
+    })
+
+    // send favorite number to front
+})
+
+router.post('/removeFromFavorite', (req, res) => {
+    // removeFromFavorite
+    Favorite.findOneAndDelete({movieId: req.body.movieId, userFrom: req.body.userFrom})
+    .exec((err, doc) => {
+        if (err) return res.status(400).send(err)
+        res.status(200).json({success: true, doc})
+    })
+})
+
+router.post('/addToFavorite', (req, res) => {
+    // addToFavorite
+    const favorite = new Favorite(req.body)
+
+    favorite.save((err, doc) => {
+        if (err) return res.status(400).send(err)
+        return res.status(200).json({success: true})
+    })
+})
+
+router.post('/getFavoredMovie', (req, res) => {
+    // get favorite movie info
+    Favorite.find({'userFrom': req.body.userFrom})
+    .exec((err, favorites) => {
+        if (err) return res.status(400).send(err)
+        return res.status(200).json({success:true, favorites})
+    })
+})
+
 module.exports = router;
